898d87a610af965a3c3f560fed870aa135761b57
MLibralato91 <michelelibralato911@gmail.com>
2023-04-17 15:28:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+<h1>ciao</h1>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    components:{
+      
+    }    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
