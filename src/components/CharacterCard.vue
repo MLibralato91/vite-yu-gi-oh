dbea5e52f49f5fd8ff786c9548fc3a7a5eb7738c
MLibralato91 <michelelibralato911@gmail.com>
2023-04-17 22:25:11 +0200

    add styles

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="col-12 col-sm-6 col-md-4 col-lg-3 pt-5" >
-      <div class="character text-center">
-          <img class="img-fluid" :src="character.card_images[0].image_url" :alt="character.name">
-          <h4>{{ character.name }}</h4>
-          <div class="font-weight-bold">{{ character.archetype }}</div>
+  <div class="col-12 col-sm-6 col-md-4 col-lg-3 pt-5 d-flex align-items-stretch">
+    <div class="character text-center d-flex flex-column justify-content-between">
+      <div class="imgCard">
+        <img class="img-fluid" :src="character.card_images[0].image_url" :alt="character.name">
+        <h4>{{ character.name }}</h4>
       </div>
+      <div class="font-weight-bold">{{ character.archetype }}</div>
+    </div>
   </div>
 </template>
 
@@ -17,12 +19,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.character{
+.character {
   background-color: rgba(169, 169, 135, 0.359);
   border-radius: 5%;
 
 }
-img{
+
+img {
   border-radius: 5%;
   box-shadow: 0 0 6px 3px rgba(#000, 0.55);
 
